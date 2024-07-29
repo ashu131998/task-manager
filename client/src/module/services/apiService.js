7c20b840796c7172f15b5e70465130e1f709c937
Ashutosh Khandelwal <ashutosh@toolboxos.com>
2024-07-29 18:14:32 +0530

    changes

--- client/src/module/services/apiService.js
+++ client/src/module/services/apiService.js
@@ -16,28 +16,28 @@ async function apiServiceHandler(method, endpoint, payload) {
       // response = await axios.get(`${baseUrl}/${endpoint}`, { config });
       response = await axios({
         method: 'get',
-        url: `${endpoint}`,
+        url: `https://server-nine-kohl.vercel.app/${endpoint}`,
         headers: { 'Authorization': 'Bearer ' + token }
       });
     } else if (method === 'POST') {
       // response = await axios.post(`${baseUrl}/${endpoint}`, payload, { config });
       response = await axios({
         method: 'post',
-        url: `${endpoint}`,
+        url: `https://server-nine-kohl.vercel.app/${endpoint}`,
         headers: { 'Authorization': 'Bearer ' + token },
         data: payload
       })
     } else if (method === 'PUT') {
       response = await axios({
         method: 'put',
-        url: `${endpoint}`,
+        url: `https://server-nine-kohl.vercel.app/${endpoint}`,
         headers: { 'Authorization': 'Bearer ' + token },
         data: payload
       })
     } else if (method === 'PATCH') {
       response = await axios({
         method: 'patch',
-        url: `${endpoint}`,
+        url: `https://server-nine-kohl.vercel.app/${endpoint}`,
         headers: { 'Authorization': 'Bearer ' + token },
         data: payload
       })
@@ -45,7 +45,7 @@ async function apiServiceHandler(method, endpoint, payload) {
     else if (method === 'DELETE') {
       response = await axios({
         method: 'delete',
-        url: `${endpoint}`,
+        url: `https://server-nine-kohl.vercel.app/${endpoint}`,
         headers: { 'Authorization': 'Bearer ' + token },
         data: payload
       })
